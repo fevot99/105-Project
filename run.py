85fd72c82e3e3c95c09682325ec65a8bd68046ff
fevot99 <fevot@yahoo.com>
2024-08-01 17:45:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,19 +8,19 @@ st.write("This is a simple example of a ITI105 app.")
 # Adding a sidebar
 st.sidebar.title("Sidebar")
 option = st.sidebar.selectbox(
-    'Select a number',
+    'Enter the number of Songs',
     list(range(1, 11))
 )
 
 # Display the selected option
-st.write(f'You selected: {option}')
+st.write(f'The number of Songs elected: {option}')
 
 # Dataframe display
 df = pd.DataFrame({
     'First Column': [1, 2, 3, 4],
-    'Second Column': [10, 20, 30, 40]
+    'Second Column': ['Song 1', 'Song 2', 'Song 3', 'Song 4']
 })
-st.write("Here's a simple dataframe:")
+st.write("Here's the Recommended Song and Artist Name:")
 st.write(df)
 
 # Plotting a chart
